4b3e1728fd878d7f115152078ece016d3efbe3c0
David Rouse <3457098+rouse99@users.noreply.github.com>
2021-03-09 11:36:13 -0500

    Module 167 - Public Routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -9,13 +9,14 @@ import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 import createHistory from 'history/createBrowserHistory';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={LoginPage} exact={true} />
+                <PublicRoute path="/" component={LoginPage} exact={true} />
                 <PrivateRoute path="/dashboard" component={ExpenseDashBoardPage} />
                 <PrivateRoute path="/create" component={AddExpensePage} />
                 <PrivateRoute path="/edit/:id" component={EditExpensePage} />
