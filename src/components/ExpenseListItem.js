75b8ac19f59d5316b1db6d84a5e20e32ceba72ca
David Rouse <3457098+rouse99@users.noreply.github.com>
2021-03-10 05:10:44 -0500

    Module 177 - Styling Expenses List - Part I

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -7,15 +7,14 @@ import numeral from 'numeral';
 
 // const ExpenseListItem = ({ dispatch, id, description, amount, createdAt}) => (
 const ExpenseListItem = ({ id, description, amount, createdAt}) => (    
-    <div>
-        <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
-        <p>
-            {numeral(amount/100).format('$0,0.00')}
-            - 
-            {moment(createdAt).format('MMMM Do, YYYY')}
-        </p>   
-        
-    </div>
+
+    <Link to={`/edit/${id}`} className="list-item">
+        <div>
+            <h3>{description}</h3>
+            <span>{moment(createdAt).format('MMMM Do, YYYY')}</span>
+        </div>
+        <h3>{numeral(amount/100).format('$0,0.00')}</h3>        
+    </Link>
 );
 
 export default ExpenseListItem;
